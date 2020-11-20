ca6bc3e6922fabc27be1f55310ddaa79d3f598f3
noraworld <mail@noraworld.com>
2020-11-20 14:20:01 +0000

    Pass through tweets to localhost:5001

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
-const port = 5000;
-const crypto = require('crypto');
+const express    = require('express');
+const app        = express();
+const port       = 5000;
+const crypto     = require('crypto');
+const request    = require('request');
+const bodyParser = require('body-parser');
+
+let request_options = {
+  method: 'POST',
+  json: true,
+  url: 'http://localhost:5001',
+  headers: {
+    'Content-type': 'application/json'
+  }
+}
 
 app.get('/webhook', (req, res) => {
   console.log('GET /webhook');
@@ -11,9 +22,25 @@ app.get('/webhook', (req, res) => {
   res.send('{"response_token":"sha256=' + hmac + '"}');
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.post('/webhook', (req, res) => {
   console.log('POST /webhook');
   res.setHeader('Content-Type', 'text/plain');
+
+  if (req.body.tweet_create_events) {
+    request_options.body = req.body.tweet_create_events;
+    request.post(request_options, (err, res, body) => {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        console.log('OK');
+      }
+    });
+  }
+
   res.sendStatus(200);
 });
 
